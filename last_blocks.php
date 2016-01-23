587a4111625ab02b864346f0beb5208d8bed887d
urodelus <urodelus@free.fr>
2016-01-23 18:41:48 +0100

    Update last_blocks.php

--- last_blocks.php
+++ last_blocks.php
@@ -1,37 +1,52 @@
-<html> 
-
+<html>
    <title>Les derniers blocks</title>
-
    <head>
+      <meta http-equiv="refresh" content="600"; url=http://localhost/uCoin/LastBlocks.php">
       <style>
+         body {color: black; font-family: Courier-new; font-size: 14;}
          .block {color: blue; font-family: Courier-new; font-size: 14;}
          .nonce {color: red; font-family: Courier-new; font-size: 14;}
-         .issuer {color: black; font-family: Courier-new; font-size: 14;}
+         .issuer {color: black; font-family: monospace; font-size: 14;}
       </style>
-   </head>
-   
+   </head>   
    <body>
       <?php
-   // Pas de retour d'erreurs (block vide...)
-         error_reporting(E_ALL);
-         ini_set('display_errors','Off');
-   // Lectures des blocks t
-         for ($t = 48550; $t>48450; $t--)
-            {
-  // Met le contenu de $adresse dans $page
-            $adresse = "http://192.168.1.50:9201/blockchain/block/".$t;
-            $page = file_get_contents ($adresse);
-  // Extrait de $page tous les caractères (.*) compris entre "issuer": " & " et les range dans le tableau $issuer
-            preg_match_all ('#"issuer": "(.*)"#', $page, $issuer);
-            preg_match_all ('#"nonce": (.*),#', $page, $nonce);
-            for($i = 0; $i < count($nonce[1]); $i++) // On parcourt le tableau $issuer[1] 'le contenu entre ()'
-               { echo '<span class="block"> ', $t, '  </span><span class="nonce">', $nonce[1][$i], '</span>'; // On affiche toute la capture
+         date_default_timezone_set("Europe/Paris"); 
+         setlocale(LC_TIME, 'fra_fra');
+         echo strftime('%A %d %B %Y %H:%M:%S S%W'), '<br>';
+      ?>
+      <table border="0" cellpadding="5" cellspacing="2" >
+      <tbody>
+         <tr><td>Block</td><td>Nonce</td><td>Issuer</td><td>Time</td><td>Pow</td></tr>      
+         <?php
+            for ($t = 48750; $t>48500; $t--)
+               {  $adresse = "http://192.168.1.94:9201/blockchain/block/".$t;
+                  $AgetHeaders = @get_headers($adresse);
+                  if (preg_match("|200|", $AgetHeaders[0]))
+                     {  $page = file_get_contents ($adresse);
+                        preg_match_all ('#"nonce": (.*),#', $page, $nonce);                     
+                        for($i = 0; $i < count($nonce[1]); $i++)
+                           { echo '<tr><td><span class="block">', $t, '</span></td><td><span class="nonce">', $nonce[1][$i], '</span></td>';
+                           }               
+                       preg_match_all ('#"issuer": "(.*)"#', $page, $issuer);
+                        for($i = 0; $i < count($issuer[1]); $i++)
+                           { echo '<td><span class="issuer">', $issuer[1][$i], '</span></td>'; 
+                           }
+                      preg_match_all ('#"time": (.*),#', $page, $time);                        
+                        $heure=""; 
+                        for($i = 0; $i < count($time[1]); $i++)
+                           { $heure.=$time[1][$i];
+                           }
+                        echo '<td><span class="nonce"> ', ''.date('d/m/Y', $heure).' '.date('H:i:s', $heure), '</span></td>';
+                       preg_match_all ('#"powMin": (.*),#', $page, $powMin);
+                        for($i = 0; $i < count($powMin[1]); $i++)
+                           { echo '<td><span class="issuer">', $powMin[1][$i], '</span></td>'; 
+                           } 
+                    }
+                  else echo ''; 
                }
-            for($i = 0; $i < count($issuer[1]); $i++)
-               { echo '<span class="issuer">  ', $issuer[1][$i], '</span>'; 
-               }
-            }
-      ?>  
+         ?>
+      </tbody>
+      </table>  
    </body>
-
-</html>
+</html>  
